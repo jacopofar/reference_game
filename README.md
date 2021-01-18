015db464098ed6dac6b7e6328f95dfa9195141ad
Jacopo Farina <jacopo.farina@flixbus.com>
2021-01-18 20:27:40 +0100

    Define a test map with 2 layers and tilesets

---
+++ README.md
@@ -0,0 +1,10 @@
+# Demo reference game
+
+This is a game intended to demo and test a game engine based on Phaser.
+This repository contains the data representing a game, not the code to run it.
+
+## Credits and assets
+The assets are from Liberated Pixel Cup, see:
+
+* https://opengameart.org/content/2d-lost-garden-tileset-transition-to-jetrels-wood-tileset
+* https://opengameart.org/content/rpg-character
---
+++ maps/first/wood_tileset.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="wood_tileset" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="wood_tileset.png" trans="f1fffa" width="512" height="512"/>
+ <wangsets>
+  <wangset name="New Wang Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="154" wangid="0,0,0,1,1,0,0,0"/>
+   <wangtile tileid="155" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="171" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="172" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="173" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="174" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="175" wangid="1,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
